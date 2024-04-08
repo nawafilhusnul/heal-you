e36ca17c495bd8cc4a4971abaffa84b9f4cc7c71
nawafilhusnul <nawafilhusnul@gmail.com>
2024-04-09 04:12:52 +0700

    chore(url-name): change url name

--- resources/views/front/carts.blade.php
+++ resources/views/front/carts.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Cart | Parma</title>
+    <title>Cart | Heal You</title>
     <link rel="shortcut icon" href="{{ asset('assets/svgs/logo-mark.svg') }}" type="image/x-icon">
     <link rel="stylesheet" href="{{ asset('css/main.css') }}">
     <link rel="stylesheet" href="https://unpkg.com/flickity@2/dist/flickity.min.css">
